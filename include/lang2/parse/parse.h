d205047c5daa50ae182a48f3d2ae1b8cee411f6c
Martin Dørum <martid0311@gmail.com>
2020-10-10 17:35:27 +0200

    style

--- include/lang2/parse/parse.h
+++ include/lang2/parse/parse.h
@@ -15,9 +15,7 @@ struct l2_parse_state {
 void l2_parse_init(
 		struct l2_parse_state *state,
 		struct l2_lexer *lexer, struct l2_io_writer *w);
-
 void l2_parse_free(struct l2_parse_state *state);
-
 void l2_parse_program(struct l2_parse_state *state);
 
 #endif
