52466b2fc0eb5ab26dd3938d1479b7126caed03d
Martin Dørum <martid0311@gmail.com>
2021-03-28 23:35:55 +0200

    compare against build.old

--- benchmarks/while-perf/bench.sh
+++ benchmarks/while-perf/bench.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-hyperfine '../../bx-out/target while-test.g' 'python3 while-test.py'
+if [ -e "../../build.old/gilia" ]; then
+	hyperfine \
+		'../../build.old/gilia while-test.g' \
+		'../../build/gilia while-test.g' \
+		'python3 while-test.py'
+else
+	hyperfine \
+		'../../build/gilia while-test.g' \
+		'python3 while-test.py'
+fi
